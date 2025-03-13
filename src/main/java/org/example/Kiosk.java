4127a237c92ab18facb033bfdf8307e1ca6fe178
정소민 <yjk70702002@gmail.com>
2025-03-13 10:46:26 +0900

    refactor: Kiosk 리팩토링

--- src/main/java/org/example/Kiosk.java
+++ src/main/java/org/example/Kiosk.java
@@ -13,6 +13,13 @@ public class Kiosk {
     }
 
     public void start() {
+        orderMenu();
+
+        // 프로그램을 종료
+        System.out.println("프로그램을 종료합니다.");
+    }
+
+    private void orderMenu() {
         int categoryNum = -1;
         while (categoryNum != 0) {
             // List와 Menu 클래스 활용하여 상위 카테고리 메뉴 출력
@@ -24,39 +31,8 @@ public class Kiosk {
 
             // 카테고리 메뉴 숫자 입력 받기
             categoryNum = InputHandler.getIntInput();
-
-            if (categoryNum > 0 && categoryNum <= menus.size()) {
-                // 입력 받은 숫자가 올바르다면 인덱스를 활용하여 Menu에 접근하기
-                Menu menu = menus.get(categoryNum - 1);
-
-                int menuNum = -1;
-                while (menuNum != 0) {
-                    // Menu가 가진 List<MenuItem>을 반복문을 활용하여 햄버거 메뉴 출력
-                    menu.printMenuItems();
-
-                    // 메뉴 숫자 입력 받기
-                    menuNum = InputHandler.getIntInput();
-
-                    if (menuNum > 0 && menuNum <= menu.getMenuItemSize()) {
-                        menu.printSelectedMenu(menuNum);
-                        if (menu.addOrder(menuNum, orders)) {
-                            break;
-                        }
-                    } else if (menuNum != 0) {
-                        System.out.println("올바르지 않은 번호입니다.");
-                    }
-                }
-            } else if (!orders.isEmpty() && categoryNum == menus.size() + 1) {
-                orders.order();
-            } else if (!orders.isEmpty() && categoryNum == menus.size() + 2) {
-                orders.clearOrders();
-            } else if (categoryNum != 0) {
-                System.out.println("올바르지 않은 번호입니다.");
-            }
+            navigateCategory(categoryNum);
         }
-
-        // 프로그램을 종료
-        System.out.println("프로그램을 종료합니다.");
     }
 
     private void printCategoryMenus() {
@@ -73,5 +49,43 @@ public class Kiosk {
         System.out.println("[ ORDER MENU ]");
         System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.");
         System.out.println("5. Cancel       | 진행중인 주문을 취소합니다.");
+    }
+
+    private void navigateCategory(int categoryNum) {
+        if (categoryNum > 0 && categoryNum <= menus.size()) {
+            // 입력 받은 숫자가 올바르다면 인덱스를 활용하여 Menu에 접근하기
+            Menu menu = menus.get(categoryNum - 1);
+            selectMenuItem(menu);
+        } else if (!orders.isEmpty() && categoryNum == menus.size() + 1) {
+            orders.order();
+        } else if (!orders.isEmpty() && categoryNum == menus.size() + 2) {
+            orders.clearOrders();
+        } else if (categoryNum != 0) {
+            System.out.println("올바르지 않은 번호입니다.");
+        }
+    }
+
+    private void selectMenuItem(Menu menu) {
+        int menuNum = -1;
+        while (menuNum != 0) {
+            // Menu가 가진 List<MenuItem>을 반복문을 활용하여 햄버거 메뉴 출력
+            menu.printMenuItems();
+
+            // 메뉴 숫자 입력 받기
+            menuNum = InputHandler.getIntInput();
+
+            // 장바구니에 추가 완료하면 반복문을 빠져나온다.
+            if (navigateMenuItem(menu, menuNum)) break;
+        }
+    }
+
+    private boolean navigateMenuItem(Menu menu, int menuNum) {
+        if (menuNum > 0 && menuNum <= menu.getMenuItemSize()) {
+            menu.printSelectedMenu(menuNum);
+            return menu.addOrder(menuNum, orders);
+        } else if (menuNum != 0) {
+            System.out.println("올바르지 않은 번호입니다.");
+        }
+        return false;
     }
 }
